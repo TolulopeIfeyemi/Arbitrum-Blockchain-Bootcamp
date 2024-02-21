c9a28224a0d57cba0b10f6e93fa4e432b835ddcf
Ifeyemi-Tolulope <toluwalopeifeyemi@gmail.com>
2024-02-21 13:53:53 +0100

    Assignment

--- Assignment 11/Nft_Marketplace.sol
+++ Assignment 11/Nft_Marketplace.sol
@@ -1,9 +1,9 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.0;
 
-import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
-import "@openzeppelin/contracts/utils/Counters.sol";
-import "@openzeppelin/contracts/access/Ownable.sol";
+// import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
+// import "@openzeppelin/contracts/utils/Counters.sol";
+// import "@openzeppelin/contracts/access/Ownable.sol";
 
 
 contract NFTMarketplace is ERC721, Ownable {
--- Assignment 13/Vulnrabilities.sol
+++ Assignment 13/Vulnrabilities.sol
@@ -1,6 +1,6 @@
 pragma solidity ^0.8.0;
 
-import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+// import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 
 contract GaslessTokenTransfer is ERC20 {
     // This mapping is used to prevent replay attacks. However, it uses the signature as the key,
